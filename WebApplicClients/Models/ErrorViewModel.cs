9a336a2e1a73f3ef65c45d2038e6f47c3d08deb9
Olviraa <olvirakoca@gmail.com>
2024-01-26 14:37:00 +0100

    Commit

---
+++ WebApplicClients/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebApplicClients.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
