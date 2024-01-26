ca73df96324918e4366cbdd7792cb36148dae9db
jmnoelison <jeanmichel.noelison@gmail.com>
2024-01-26 14:22:13 +0100

    Creation des modeles

---
+++ ModelsCommun/Produit.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ModelsCommun
+{
+    public class Produit
+    {
+        // Id du produit
+        [JsonPropertyName("id")]
+        public int ID { get; set; }
+        // Nom du produit
+        [JsonPropertyName("nom")]
+        public string Nom { get; set; }
+        // Prix du produit
+        [JsonPropertyName("prix")]
+        public double Prix { get; set; }
+        // Quantite disponible du produit
+        [JsonPropertyName("quantitedisponible")]
+        public int QuantiteDisponible { get; set; }
+    }
+}
---
+++ ModelsCommun/ProduitVendu.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace ModelsCommun
+{
+    public class ProduitVendu
+    {
+        // Id de produit vendu ou produit par vente
+        [JsonPropertyName("id")]
+        public int ID { get; set; }
+
+        // La quantité vendu
+        [JsonPropertyName("quantitevendue")]
+        public int QuantiteVendue { get; set; }
+
+        // et le produit en question pour les propriétés (pour faire comprendre à EntityFramework)
+        [JsonPropertyName("produit")]
+        public Produit Produit { get; set; }
+    }
+}
