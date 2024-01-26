1118bb23f44c5542f0e6e1ac1ca1658ea4302e2c
Thomas-68 <thomas.litzler68@gmail.com>
2024-01-26 16:22:09 +0100

    Esquisse Appli Admin

---
+++ WebAppAdmin/Controllers/AdminController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAppAdmin.Controllers
+{
+    [Route("/")]
+    public class AdminController : Controller
+    {
+        // GET: AdminController
+        [Route("")]
+        public ActionResult AdminHome()
+        {
+            return View();
+        }
+        //[Route("Admin")]
+        //public ActionResult AdminHome()
+        //{
+        //    return View();
+        //}
+        [HttpGet]
+        [Route("Articles")]
+        public ActionResult ListeArticles()
+        {
+            return View();
+        }
+        
+    }
+}
