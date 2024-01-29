05dab19c1b2a3cffba9be6bed601f4857e9cdc4b
jurgaa <jurgaval7@gmail.com>
2024-01-29 11:29:09 +0100

    Api-ListProduit

---
+++ ApiVenteArticles/Controllers/VenteController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiVenteArticles.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VenteController : ControllerBase
+    {
+    }
+}
---
+++ ApiVenteArticles/Services/ProduitService.cs
@@ -0,0 +1,59 @@
+﻿using ApiVenteArticles.Interface;
+using ApiVenteArticles.Repositories;
+using ModelsCommun;
+
+namespace ApiVenteArticles.Services
+{
+    public class ProduitService: IProduitService
+    {
+
+        private readonly ApiDbContext _dbContext;
+
+        public ProduitService(ApiDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public Produit AddProduct(Produit newProduct)
+        {
+            _dbContext.Produits.Add(newProduct);
+            _dbContext.SaveChanges();
+
+            return newProduct;
+        }
+
+        public List<Produit> GetProducts()
+        {
+            return _dbContext.Produits.ToList();
+        }
+
+        public Produit GetProduct(int id)
+        {
+            return _dbContext.Produits.FirstOrDefault(p => p.ID == id);
+        }
+
+        public Produit UpdateProduct(int id, string nom, double prix, int quantiteDispo)
+        {
+            Produit productToUpdate = _dbContext.Produits.FirstOrDefault(p => p.ID == id);
+            productToUpdate.Nom = nom;
+            productToUpdate.Prix = prix;
+            productToUpdate.QuantiteDisponible = quantiteDispo;
+
+            _dbContext.SaveChanges();
+
+            return productToUpdate;
+        }
+
+        public void DeleteProduct(int id)
+        {
+
+            Produit productToDelete = _dbContext.Produits.FirstOrDefault(p => p.ID == id);
+            _dbContext.Produits.Remove(productToDelete);
+            _dbContext.SaveChanges();
+
+          
+
+        }
+
+    }
+}
