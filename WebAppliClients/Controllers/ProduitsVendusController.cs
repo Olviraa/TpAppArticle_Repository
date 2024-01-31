6e8b2b288e5bd2e59f0069f197326c8bf2c19d45
Olviraa <olvirakoca@gmail.com>
2024-01-31 15:05:29 +0100

    commit (help)

--- WebAppliClients/Controllers/ProduitsVendusController.cs
+++ WebAppliClients/Controllers/ProduitsVendusController.cs
@@ -14,6 +14,7 @@ namespace WebAppliClients.Controllers
        public IActionResult AddProduitPanier(int IdProduit, int IdVente, int Quantite)
        {
             // RECEPTION DU produitVendu DU PRODUITVENDUSERVICE
+
             ProduitVenduService produitVenduReçu = new ProduitVenduService();
             var produitVenduTosend = produitVenduReçu.AddProduitPanier(IdProduit, IdVente, Quantite);
 
